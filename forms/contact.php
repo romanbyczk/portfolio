56fd917bf5678b530ad905e9c239bcb02ce56fb8
Administrator <pianistamichal@gmail.com>
2020-11-04 15:22:40 +0100

    Corrected contact form and changed portfolio links

--- forms/contact.php
+++ forms/contact.php
@@ -6,14 +6,38 @@
   * For more info and help: https://bootstrapmade.com/php-email-form/
   */
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'rbyczk94@gmail.com';
 
+  function sanitize_my_email($field) {
+    $field = filter_var($field, FILTER_SANITIZE_EMAIL);
+    if (filter_var($field, FILTER_VALIDATE_EMAIL)) {
+        return true;
+    } else {
+        return false;
+    }
+  }
+  
+  $subject = $_POST['subject'];
+  $message = $_POST['message'];
+  $headers = 'From: ' + $_POST['email'];
+  if(sanitize_my_email($_POST['email']))
+  {
+    mail($receiving_email_address,filter_var($subject, FILTER_SANITIZE_STRING),filter_var($message, FILTER_SANITIZE_STRING),$headers);
+  }
+  else 
+  {
+    echo "ERROR IN EMAIL";
+  }
+
+
+/*
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
-  }
+  }*/
+
+/*
 
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
@@ -22,6 +46,7 @@
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
+*/
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
@@ -32,10 +57,12 @@
     'port' => '587'
   );
   */
-
+/*
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
   echo $contact->send();
+
+  */
 ?>
